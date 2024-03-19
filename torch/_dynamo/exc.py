8b6964a923ceb6e8523e5ebe533994a9026f7ef2
Peter Bell <peterbell10@live.co.uk>
2024-03-19 17:27:48 +0000

    [dynamo] Remove uses of `raise unimplemented`
    `unimplemented` is a function that raises an error, so
    `raise unimplemented(...)` never reaches the `raise`.
    Another related issue is that `raise unimplemented(...) from e`
    doesn't attach the exception cause correctly. I fix this by adding
    a `from_exc` argument to `unimplemented`.
    
    ghstack-source-id: 495184ce1d1b22a792bb0310b3e07757fbfa7864
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/122136

--- torch/_dynamo/exc.py
+++ torch/_dynamo/exc.py
@@ -2,7 +2,7 @@ import os
 import textwrap
 from enum import auto, Enum
 from traceback import extract_stack, format_exc, format_list, StackSummary
-from typing import cast, NoReturn, Optional
+from typing import cast, Literal, NoReturn, Optional, Union
 
 import torch._guards
 
@@ -182,11 +182,18 @@ def unimplemented_with_warning(e: Exception, code, msg: str) -> NoReturn:
     graph_break_msg = format_error_msg_verbose(e, code)
     graph_breaks_log.debug("%s", graph_break_msg)
     log.warning(msg)
-    raise unimplemented(msg) from e
+    unimplemented(msg, from_exc=e)
 
 
-def unimplemented(msg: str) -> NoReturn:
+_NOTHING = object()
+
+
+def unimplemented(
+    msg: str, *, from_exc: Union[Optional[Exception], Literal[_NOTHING]] = _NOTHING
+) -> NoReturn:
     assert msg != os.environ.get("BREAK", False)
+    if from_exc is not _NOTHING:
+        raise Unsupported(msg) from from_exc
     raise Unsupported(msg)
 
 
